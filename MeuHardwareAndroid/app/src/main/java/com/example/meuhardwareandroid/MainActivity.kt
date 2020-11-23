b791e96e421c71230498c02bbfd55d7588aee144
kikepola <kikepola@gmail.com>
2020-11-22 22:03:07 -0300

    Adding menu options

--- MeuHardwareAndroid/app/src/main/java/com/example/meuhardwareandroid/MainActivity.kt
+++ MeuHardwareAndroid/app/src/main/java/com/example/meuhardwareandroid/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val gcBtn = findViewById<Button>(R.id.gcBtn)
+        val gcBtn = findViewById<ImageButton>(R.id.gpuBtn)
 
         gcBtn.setOnClickListener {
             val intent = Intent(this, GraphicCardActivity::class.java)
