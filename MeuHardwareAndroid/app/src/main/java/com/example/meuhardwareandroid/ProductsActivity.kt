0bd8dc04cc3792f3ec1eeebfb8df8b5c80d56e4a
kikepola <kikepola@gmail.com>
2020-11-29 22:32:04 -0300

    Order by on Product Activity

--- MeuHardwareAndroid/app/src/main/java/com/example/meuhardwareandroid/ProductsActivity.kt
+++ MeuHardwareAndroid/app/src/main/java/com/example/meuhardwareandroid/ProductsActivity.kt
@@ -7,9 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.*
 import androidx.lifecycle.ViewModelProvider
 import com.example.meuhardwareandroid.adapters.ProductAdapter
 import com.example.meuhardwareandroid.model.Product
@@ -24,18 +22,21 @@ class ProductsActivity : AppCompatActivity() {
     private lateinit var listView: ListView
     private lateinit var productNameTxt: TextView
     private lateinit var progressBar: ProgressBar
+    private lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
+        setOrdeByProductsSpinner()
 
         var option: String = intent.getStringExtra("option")
+
+        spinner = setOrdeByProductsSpinner()
 
         listView = findViewById<ListView>(R.id.product_list_view)
 
         progressBar = findViewById<ProgressBar>(R.id.progressBarProducts)
         progressBar.visibility = View.VISIBLE
-
 
         productNameTxt = findViewById<TextView>(R.id.productTxt)
         productNameTxt.text = option
@@ -52,6 +53,20 @@ class ProductsActivity : AppCompatActivity() {
             "Processador" -> viewModel.getProcessor()
         }
 
+        displayProductList()
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                displayProductList(position)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    fun displayProductList(orderOption: Int = 1){
         viewModel.response.observe(this, Observer { response ->
             if (response.isSuccessful) {
                 // val listItems = response.body()
@@ -59,7 +74,14 @@ class ProductsActivity : AppCompatActivity() {
                 var listItems = ArrayList<Product>()
                 response.body()?.forEach { graphicCard: Product -> listItems.add(graphicCard) }
 
-                val adapter = ProductAdapter(this, listItems)
+                var orderedList: ArrayList<Product> = ArrayList<Product>()
+                when(orderOption){
+                    0 -> orderedList = oderByMaxPrice(listItems)
+                    1 -> orderedList = oderByMinPrice(listItems)
+                    2 -> orderedList = oderByName(listItems)
+                }
+
+                val adapter = ProductAdapter(this, orderedList)
                 listView.adapter = adapter
 
                 val params: ViewGroup.LayoutParams = listView.layoutParams
@@ -80,5 +102,30 @@ class ProductsActivity : AppCompatActivity() {
             val i = Intent(Intent.ACTION_VIEW, Uri.parse(element.link))
             startActivity(i)
         }
+    }
+
+    fun setOrdeByProductsSpinner(): Spinner{
+        val spinner: Spinner = findViewById(R.id.orderBySpinner)
+
+        val oderByAdapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this, R.array.orderByItems, android.R.layout.simple_spinner_item)
+        oderByAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = oderByAdapter
+        return spinner
+    }
+
+    fun oderByMaxPrice(products: ArrayList<Product>): ArrayList<Product> {
+        val result = products.sortedByDescending { it.price }
+        return ArrayList(result)
+    }
+
+    fun oderByMinPrice(products: ArrayList<Product>): ArrayList<Product> {
+        val result = products.sortedBy { it.price }
+        return ArrayList(result)
+    }
+
+    fun oderByName(products: ArrayList<Product>): ArrayList<Product> {
+        val result = products.sortedBy { it.storeName }
+        return ArrayList(result)
     }
 }
