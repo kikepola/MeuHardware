16d862086f0b01a323caac96f862d45122609d2b
kikepola <kikepola@gmail.com>
2021-01-06 23:26:43 -0300

    Creating order by filter

--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -10,7 +10,18 @@ import { Router } from '@angular/router';
 export class ProductsPageComponent implements OnInit {
 
   products: any;
+  result: any;
   isLoading: boolean = false;
+  filters: any = [
+    {
+      option: 0,
+      text: "Preço decrescente"
+    },
+    {
+      option: 1,
+      text: "Preço crescente"
+    }
+  ]
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -22,11 +33,33 @@ export class ProductsPageComponent implements OnInit {
     this.isLoading = true;
     this.http.get('https://meuhardware-backend.herokuapp.com' + this.router.url)
     .subscribe(response => {
-      this.products = response;
+      this.result = response;
+      this.products =this.result;
+
       this.isLoading = false;
     }, error => {
       console.log(error);
     });
+  }
+
+
+  orderBy(option: number){
+    this.products = this.result.sort((product1:any, product2:any) => {
+        var referenceNumber: number = 0;
+
+        if(product1.price > product2.price){
+          referenceNumber = 1;
+        }else if(product1.price < product2.price){
+          referenceNumber =  -1;
+        }
+
+        if(option == 0){
+          referenceNumber = referenceNumber * -1
+        }
+
+        return referenceNumber;
+      }
+    )
   }
 
 }
